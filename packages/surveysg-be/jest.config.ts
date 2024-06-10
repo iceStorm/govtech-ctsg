63ba80ece2334e24bec60b3350c3be104e50471e
Anh Tuan <tuan.nguyen.anh@xyz.com>
2024-06-10 23:26:25 +0700

    rename packages

--- packages/surveysg-be/jest.config.ts
+++ packages/surveysg-be/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'backend',
+  displayName: 'surveysg-be',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/backend',
+  coverageDirectory: '../../coverage/packages/surveysg-be',
 };
