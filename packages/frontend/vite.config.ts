68334d88033f2a2046541eb2287f95c28d0213a9
Anh Tuan <tuan.nguyen.anh@xyz.com>
2024-06-10 13:31:50 +0700

    add common lib

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   cacheDir: '../../node_modules/.vite/packages/frontend',
 
   server: {
-    port: 4200,
+    port: 7502,
     host: 'localhost',
   },
 
   preview: {
-    port: 4300,
+    port: 7520,
     host: 'localhost',
   },
 
