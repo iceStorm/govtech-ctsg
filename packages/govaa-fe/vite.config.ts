8f5bd82425900826f2564e4cda69e5cc2518d8ed
Anh Tuan <tuan.nguyen.anh@xyz.com>
2024-06-10 23:29:07 +0700

    add govaa-fe app

---
+++ packages/govaa-fe/vite.config.ts
@@ -0,0 +1,54 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/govaa-fe',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [react(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../../dist/packages/govaa-fe',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+
+  define: {
+    'import.meta.vitest': undefined,
+  },
+  test: {
+    watch: false,
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest/packages/govaa-fe',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    includeSource: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/packages/govaa-fe',
+      provider: 'v8',
+    },
+  },
+});
